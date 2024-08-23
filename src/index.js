97d047afc7e5a645212031e7a41fdbf25b093a0b
João Vitor <123218323+jvssvj@users.noreply.github.com>
2024-08-23 20:58:54 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,82 +4,46 @@ let monthlyBtn = document.querySelector('#monthly--btn')
 
 fetch('data.json').then((response) => {
     response.json().then((info) => {
-        
-        function daily() {
-            dailyBtn.style.color = '#ffffff'
-            weeklyBtn.style.color = 'hsl(235, 45%, 61%)'
-            monthlyBtn.style.color = 'hsl(235, 45%, 61%)'
 
-
-            workCurrentHours = document.querySelector('.work__hours').innerHTML = info[0].timeframes.daily.current + 'hrs'
-            workLastHours = document.querySelector('.work__last__hours').innerHTML = info[0].timeframes.daily.previous + 'hrs'
-
-            playCurrentHours = document.querySelector('.play__hours').innerHTML = info[1].timeframes.daily.current + 'hrs'
-            playLastHours = document.querySelector('.play__last__hours').innerHTML = info[1].timeframes.daily.previous + 'hrs'
-
-            studyCurrentHours = document.querySelector('.study__hours').innerHTML = info[2].timeframes.daily.current + 'hrs'
-            studyLastHours = document.querySelector('.study__last__hours').innerHTML = info[2].timeframes.daily.previous + 'hrs'
-
-            exerciseCurrentHours = document.querySelector('.exercise__hours').innerHTML = info[3].timeframes.daily.current + 'hrs'
-            exerciseLastHours = document.querySelector('.exercise__last__hours').innerHTML = info[3].timeframes.daily.previous + 'hrs'
-
-            socialCurrentHours = document.querySelector('.social__hours').innerHTML = info[4].timeframes.daily.current + 'hrs'
-            socialLastHours = document.querySelector('.social__last__hours').innerHTML = info[4].timeframes.daily.previous + 'hrs'
-
-            selfcareCurrentHours = document.querySelector('.selfcare__hours').innerHTML = info[5].timeframes.daily.current + 'hrs'
-            selfcareLastHours = document.querySelector('.selfcare__last__hours').innerHTML = info[5].timeframes.daily.previous + 'hrs'
+        function colorBtn (dailyBtn, weeklyBtn, monthlyBtn, dailyBtnColor, weeklyBtnColor, monthlyBtnColor) {
+            dailyBtn.style.color = dailyBtnColor
+            weeklyBtn.style.color = weeklyBtnColor
+            monthlyBtn.style.color = monthlyBtnColor
         }
 
-        function weekly() {
-            dailyBtn.style.color = 'hsl(235, 45%, 61%)'
-            weeklyBtn.style.color = '#ffffff'
-            monthlyBtn.style.color = 'hsl(235, 45%, 61%)'
-
-            workCurrentHours = document.querySelector('.work__hours').innerHTML = info[0].timeframes.weekly.current + 'hrs'
-            workLastHours = document.querySelector('.work__last__hours').innerHTML = info[0].timeframes.weekly.previous + 'hrs'
-
-            playCurrentHours = document.querySelector('.play__hours').innerHTML = info[1].timeframes.weekly.current + 'hrs'
-            playLastHours = document.querySelector('.play__last__hours').innerHTML = info[1].timeframes.weekly.previous + 'hrs'
-
-            studyCurrentHours = document.querySelector('.study__hours').innerHTML = info[2].timeframes.weekly.current + 'hrs'
-            studyLastHours = document.querySelector('.study__last__hours').innerHTML = info[2].timeframes.weekly.previous + 'hrs'
-
-            exerciseCurrentHours = document.querySelector('.exercise__hours').innerHTML = info[3].timeframes.weekly.current + 'hrs'
-            exerciseLastHours = document.querySelector('.exercise__last__hours').innerHTML = info[3].timeframes.weekly.previous + 'hrs'
-
-            socialCurrentHours = document.querySelector('.social__hours').innerHTML = info[4].timeframes.weekly.current + 'hrs'
-            socialLastHours = document.querySelector('.social__last__hours').innerHTML = info[4].timeframes.weekly.previous + 'hrs'
-
-            selfcareCurrentHours = document.querySelector('.selfcare__hours').innerHTML = info[5].timeframes.weekly.current + 'hrs'
-            selfcareLastHours = document.querySelector('.selfcare__last__hours').innerHTML = info[5].timeframes.weekly.previous + 'hrs'
+        function getData(hoursContainerClass, indexDataHours, period, lastWeekContainer, indexDataLastHours) {
+            document.querySelector(`${hoursContainerClass}`).innerHTML = `${info[indexDataHours].timeframes[period].current} hrs`
+            document.querySelector(`${lastWeekContainer}`).innerHTML = `${info[indexDataLastHours].timeframes[period].previous} hrs`
         }
 
-        function monthly() {
-            dailyBtn.style.color = 'hsl(235, 45%, 61%)'
-            weeklyBtn.style.color = 'hsl(235, 45%, 61%)'
-            monthlyBtn.style.color = '#ffffff'
+        dailyBtn.addEventListener('click', () => {
+            colorBtn(dailyBtn, weeklyBtn, monthlyBtn, '#ffffff', 'hsl(235, 45%, 61%)', 'hsl(235, 45%, 61%)')
+            getData('.work__hours', 0, 'daily', '.work__last__hours', 0)
+            getData('.play__hours', 1, 'daily', '.play__last__hours', 1)
+            getData('.study__hours', 2, 'daily', '.study__last__hours', 2)
+            getData('.exercise__hours', 3, 'daily', '.exercise__last__hours', 3)
+            getData('.social__hours', 4, 'daily', '.social__last__hours', 4)
+            getData('.selfcare__hours', 5, 'daily', '.selfcare__last__hours', 5)
+        })
 
-            workCurrentHours = document.querySelector('.work__hours').innerHTML = info[0].timeframes.monthly.current + 'hrs'
-            workLastHours = document.querySelector('.work__last__hours').innerHTML = info[0].timeframes.monthly.previous + 'hrs'
+        weeklyBtn.addEventListener('click', () => {
+            colorBtn(dailyBtn, weeklyBtn, monthlyBtn, 'hsl(235, 45%, 61%)', '#ffffff', 'hsl(235, 45%, 61%)')
+            getData('.work__hours', 0, 'weekly', '.work__last__hours', 0)
+            getData('.play__hours', 1, 'weekly', '.play__last__hours', 1)
+            getData('.study__hours', 2, 'weekly', '.study__last__hours', 2)
+            getData('.exercise__hours', 3, 'weekly', '.exercise__last__hours', 3)
+            getData('.social__hours', 4, 'weekly', '.social__last__hours', 4)
+            getData('.selfcare__hours', 5, 'weekly', '.selfcare__last__hours', 5)
+        })
 
-            playCurrentHours = document.querySelector('.play__hours').innerHTML = info[1].timeframes.monthly.current + 'hrs'
-            playLastHours = document.querySelector('.play__last__hours').innerHTML = info[1].timeframes.monthly.previous + 'hrs'
-
-            studyCurrentHours = document.querySelector('.study__hours').innerHTML = info[2].timeframes.monthly.current + 'hrs'
-            studyLastHours = document.querySelector('.study__last__hours').innerHTML = info[2].timeframes.monthly.previous + 'hrs'
-
-            exerciseCurrentHours = document.querySelector('.exercise__hours').innerHTML = info[3].timeframes.monthly.current + 'hrs'
-            exerciseLastHours = document.querySelector('.exercise__last__hours').innerHTML = info[3].timeframes.monthly.previous + 'hrs'
-
-            socialCurrentHours = document.querySelector('.social__hours').innerHTML = info[4].timeframes.monthly.current + 'hrs'
-            socialLastHours = document.querySelector('.social__last__hours').innerHTML = info[4].timeframes.monthly.previous + 'hrs'
-
-            selfcareCurrentHours = document.querySelector('.selfcare__hours').innerHTML = info[5].timeframes.monthly.current + 'hrs'
-            selfcareLastHours = document.querySelector('.selfcare__last__hours').innerHTML = info[5].timeframes.monthly.previous + 'hrs'
-        }
-        
-        dailyBtn.addEventListener('click', daily)
-        weeklyBtn.addEventListener('click', weekly)
-        monthlyBtn.addEventListener('click', monthly)
+        monthlyBtn.addEventListener('click', () => {
+            colorBtn(dailyBtn, weeklyBtn, monthlyBtn, 'hsl(235, 45%, 61%)', 'hsl(235, 45%, 61%)', '#ffffff')
+            getData('.work__hours', 0, 'monthly', '.work__last__hours', 0)
+            getData('.play__hours', 1, 'monthly', '.play__last__hours', 1)
+            getData('.study__hours', 2, 'monthly', '.study__last__hours', 2)
+            getData('.exercise__hours', 3, 'monthly', '.exercise__last__hours', 3)
+            getData('.social__hours', 4, 'monthly', '.social__last__hours', 4)
+            getData('.selfcare__hours', 5, 'monthly', '.selfcare__last__hours', 5)
+        })
     })
 })
